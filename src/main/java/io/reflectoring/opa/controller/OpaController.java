aca0e5bd47206432d0d11ceba77187a279689b35
simon.meng <simon.meng@foxmail.com>
2024-09-18 23:57:59 +0800

    b

--- src/main/java/io/reflectoring/opa/controller/OpaController.java
+++ src/main/java/io/reflectoring/opa/controller/OpaController.java
@@ -9,6 +9,7 @@ import io.reflectoring.opa.model.request.GetRowFiltersResponse;
 import io.reflectoring.opa.model.Result;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,21 +18,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class OpaController {
 
     @PostMapping("/allow")
-    public ResponseEntity<AllowResponse> allow(AllowRequest allowRequest) {
+    public ResponseEntity<AllowResponse> allow(@RequestBody  AllowRequest allowRequest) {
         return ResponseEntity.ok(AllowResponse.allow());
     }
 
-
     @PostMapping("/getRowFilters")
-    public ResponseEntity<GetRowFiltersResponse> getRowFilters(GetRowFiltersRequest getRowFiltersRequest) throws JsonProcessingException {
+    public ResponseEntity<GetRowFiltersResponse> getRowFilters(@RequestBody  GetRowFiltersRequest getRowFiltersRequest) throws JsonProcessingException {
 
         // Get User group
         // Get Resource/Permission Mapping
-
-        // 1. is admin , return all data , all = true
-        // 2. get granted regions?   all ? all = true,  / all = false filter by granted region
-        // 3. get granted sites?     all ?
-        // 4. 1=2
 
         ObjectMapper objectMapper = new ObjectMapper();
 
@@ -41,19 +36,22 @@ public class OpaController {
                 {
                   "result": [
                     {
-                      "expression": " site_id = 'LOH'"
+                      "expression": " mktsegment = 'MACHINERY'"
                     }
                   ]
                 }
                 """;
 
-        GetRowFiltersResponse response = objectMapper.readValue(jsonString, GetRowFiltersResponse.class);
-
-        // Print out the expression to verify
-        for (Result result : response.getResult()) {
-            System.out.println("Expression: " + result.getExpression());
+        if (getRowFiltersRequest.getInput().getAction().getResource().getTable().getTableName().equals("customer")) {
+            GetRowFiltersResponse response = objectMapper.readValue(jsonString, GetRowFiltersResponse.class);
+            // Print out the expression to verify
+            for (Result result : response.getResult()) {
+                System.out.println("Expression: " + result.getExpression());
+            }
+            return ResponseEntity.ok(response);
         }
 
-        return ResponseEntity.ok(response);
+
+        return ResponseEntity.ok(new GetRowFiltersResponse());
     }
 }
